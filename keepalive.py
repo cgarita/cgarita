c93ea3eba02eb42269da6e9771b26cedfa451d42
cgarita <cgarita@una.cr>
2014-10-30 10:30:21 -0600

    Mantiene Leereq arriba

---
+++ keepalive.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+import commands, subprocess, os, string
+while (True):
+       	os.system('cd /opt/ProgramaEQ')
+       	os.system('/usr/bin/python2.6 /opt/ProgramaEQ/leereq.py &')
+	os.system('sleep 60')
